9369c9f05a6149c2a92d52a30b7ce1c3ec8188d2
Harriette Berndes <harrietteberndes@bright-os-01.mynet>
2021-08-11 13:22:02 +0100

    removed the for loop duplication and altered current tests

---
+++ places to visit app/Helpers/WishListStoreHelper.swift
@@ -0,0 +1,25 @@
+//
+//  WishListStoreHelper.swift
+//  places to visit app
+//
+//  Created by Harriette Berndes on 10/08/2021.
+//
+
+import Foundation
+
+class WishListStoreHelper {
+    
+    static func checkForDuplication <T, S: Equatable>(itemToCheckFor: T, listToCheckThrough: [T], propertiesToCheckAgainst: [KeyPath<T, S>]) -> Bool {
+        for item in listToCheckThrough {
+            for property in propertiesToCheckAgainst {
+                //if itemToCheckFor.property == item.property {
+                if item[keyPath: property] == itemToCheckFor[keyPath: property] {
+                    print("items checked through, round a match")
+                    return true
+                }
+            }
+        }
+        print("items checked through, no matches present")
+        return false
+    }
+}
